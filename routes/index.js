19dbbc024d3538155dd5a5546812781d332ed5e9
sshinohs <sshinohs@gmail.com>
2022-04-04 21:08:18 +0900

    주식 10개 까지 띄움.. 리팩토링 하자..

--- routes/index.js
+++ routes/index.js
@@ -12,10 +12,27 @@ ham = axios.get('https://finance.naver.com/sise/',{responseType: 'arraybuffer',r
       king = temp;
       // console.log(king);
       $ = cheerio.load(king);
-      stock_name = $('#siselist_tab_7 > tbody > tr:nth-child(3) > td:nth-child(2) > a').text();
-      stock_price = $('#siselist_tab_7 > tbody > tr:nth-child(3) > td:nth-child(3)').text();
-      console.log(stock_name);
-      console.log(stock_price);
+      stock_name_1 = $('#siselist_tab_7 > tbody > tr:nth-child(3) > td:nth-child(2) > a').text();
+      stock_price_1 = $('#siselist_tab_7 > tbody > tr:nth-child(3) > td:nth-child(3)').text();
+        stock_name_2 = $('#siselist_tab_7 > tbody > tr:nth-child(4) > td:nth-child(2) > a').text();
+        stock_price_2 = $('#siselist_tab_7 > tbody > tr:nth-child(4) > td:nth-child(3)').text();
+        stock_name_3 = $('#siselist_tab_7 > tbody > tr:nth-child(5) > td:nth-child(2) > a').text();
+        stock_price_3 = $('#siselist_tab_7 > tbody > tr:nth-child(5) > td:nth-child(3)').text();
+        stock_name_4 = $('#siselist_tab_7 > tbody > tr:nth-child(6) > td:nth-child(2) > a').text();
+        stock_price_4 = $('#siselist_tab_7 > tbody > tr:nth-child(6) > td:nth-child(3)').text();
+        stock_name_5 = $('#siselist_tab_7 > tbody > tr:nth-child(7) > td:nth-child(2) > a').text();
+        stock_price_5 = $('#siselist_tab_7 > tbody > tr:nth-child(7) > td:nth-child(3)').text();
+        stock_name_6 = $('#siselist_tab_7 > tbody > tr:nth-child(11) > td:nth-child(2) > a').text();
+        stock_price_6 = $('#siselist_tab_7 > tbody > tr:nth-child(11) > td:nth-child(3)').text();
+        stock_name_7 = $('#siselist_tab_7 > tbody > tr:nth-child(12) > td:nth-child(2) > a').text();
+        stock_price_7 = $('#siselist_tab_7 > tbody > tr:nth-child(12) > td:nth-child(3)').text();
+        stock_name_8 = $('#siselist_tab_7 > tbody > tr:nth-child(13) > td:nth-child(2) > a').text();
+        stock_price_8 = $('#siselist_tab_7 > tbody > tr:nth-child(13) > td:nth-child(3)').text();
+        stock_name_9 = $('#siselist_tab_7 > tbody > tr:nth-child(14) > td:nth-child(2) > a').text();
+        stock_price_9 = $('#siselist_tab_7 > tbody > tr:nth-child(14) > td:nth-child(3)').text();
+        stock_name_10 = $('#siselist_tab_7 > tbody > tr:nth-child(15) > td:nth-child(2) > a').text();
+        stock_price_10 = $('#siselist_tab_7 > tbody > tr:nth-child(15) > td:nth-child(3)').text();
+
       router.get('/', function(req, res, next) {
         res.render('index', { title: 'Express' });
       });
