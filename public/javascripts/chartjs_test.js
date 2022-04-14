660f0a25669ed316f7642a113e61dda6cc34c48c
sshinohs <sshinohs@gmail.com>
2022-04-14 23:28:05 +0900

    chartjs 동적으로 넣기 실패

--- public/javascripts/chartjs_test.js
+++ public/javascripts/chartjs_test.js
@@ -1,38 +1,43 @@
-const ctx = document.getElementById('myChart');
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: 'Price',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
+let ddong = 'ddong'
+let data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+        label: 'Price',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+    }]
+}
+
+let type = 'bar';
+let options = {
+    scales: {
+        y: {
+            beginAtZero: true
         }
     }
-});
+}
+let ham = JSON.parse(JSON.stringify(data));
+// document.write(ham);
+// document.write(data);
+// document.write(type);
+const ctx = document.getElementById('myChart');
+let myChart = new Chart(ctx, {type: type, data: ham, options: options});
 const config = {
     type: 'line',
     data: data,
--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,46 @@ function authIsOwner(req, res) {
     }
 }
 
+let data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+        label: 'Price',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+    }]
+}
+
+let type = 'bar';
+let options = {
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    }
+}
+
+let hham = JSON.parse(JSON.stringify(data))
+
+// const ctx = document.getElementById('myChart');
+// const myChart = new Chart(ctx, {type: type, data: data, options: options});
+
+
 router.get('/', function(req, res, next) {
     const arrNumber = [3, 4, 5, 6, 7, 11, 12, 13, 14, 15]
     let stock_name = []
@@ -26,8 +66,14 @@ router.get('/', function(req, res, next) {
                 stock_name.push($(`#siselist_tab_7 > tbody > tr:nth-child(${arrNumber[i]}) > td:nth-child(2) > a`).text())
                 stock_price.push($(`#siselist_tab_7 > tbody > tr:nth-child(${arrNumber[i]}) > td:nth-child(3)`).text())
             }
-            isLogined = authIsOwner(req, res);
-            res.render('index', { title: 'Stock ML', isLogined, stock_name, stock_price });
+            res.locals.isLogined = authIsOwner(req, res);
+            res.locals.stock_name = stock_name;
+            res.locals.stock_price = stock_price;
+            res.locals.data = data;
+            res.locals.type = type;
+            res.locals.options = options;
+            res.locals.hham = hham;
+            res.render('index', { title: 'Stock ML'});
         })
 });
 
